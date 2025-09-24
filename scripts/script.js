de167085d88acaeac37e89888912270c6f2abb93
Joey Schouten <joey.schouten.schouten@hva.nl>
2025-09-24 14:07:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,49 @@
 // JavaScript Document
 console.log("hi");
+
+const searchToggle = document.querySelector('.search-toggle');
+const searchDesktop = document.querySelector('.search-desktop');
+
+searchToggle.addEventListener('click', () => {
+    searchDesktop.classList.toggle('visible');
+});
+
+// HAMBURGER MENU
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  hamburger.classList.toggle('open');
+  overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+  nav.classList.remove('active');
+  hamburger.classList.remove('open');
+  overlay.classList.remove('active');
+});
+
+// ARTICLES SHOWEN OP KLIK
+const menuItems = document.querySelectorAll('.stappen-menu li');
+const articles = document.querySelectorAll('.items article');
+const details = document.querySelector('.stappen-menu');
+
+// standaard: eerste artikel zichtbaar
+articles[0].classList.add('active');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const articleNumber = item.getAttribute('data-article');
+    
+    articles.forEach(article => {
+      if (article.getAttribute('data-article') === articleNumber) {
+        article.classList.add('active');
+      } else {
+        article.classList.remove('active');
+      }
+    });
+    details.removeAttribute('open');
+  });
+});
